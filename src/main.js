0c51b5f7c97e12836e4e8f734e85d017b913198b
Cesar Level <cesarlevel@gmail.com>
2022-11-10 11:37:38 -0500

    Update router

--- src/main.js
+++ src/main.js
@@ -6,3 +6,17 @@ import router from './router';
 createApp(App)
     .use(router)
     .mount('#app');
+
+let { redirect } = sessionStorage;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.pathname) {
+
+    redirect = redirect.replace(import.meta.env.BASE_URL, '/');
+
+    if (config.debug) {
+        console.log('Going to redirect to ' + redirect);
+    }
+
+    router.replace(redirect);
+}
